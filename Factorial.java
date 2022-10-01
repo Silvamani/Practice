321853546f63eb6535686ce5eb51f04284ef52ce
Silvamani <silvakuricheti2000@gmail.com>
2022-10-01 15:21:10 +0530

    ignore file

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int a = 5,b=0;
+        for (int i = 1; i <= 5; i++) {
+            b = a * i;
+        }
+        System.out.println(b);
+    }
+}
